5679271e458f4df870ffd33c83b3150f865104dd
ManamiHayakawa <133200318+ManamiHayakawa@users.noreply.github.com>
2023-07-18 06:52:31 +0900

    7.18
    head.htmlを消しました

--- TeamDevelopment_2307/src/main/java/com/example/demo/controller/HousingController14.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/HousingController14.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.config.HousingRequest14;
@@ -31,16 +33,19 @@ public class HousingController14 {
 	 * @param  model Model
 	 * @return  住宅登録管理画面
 	 */
-	@RequestMapping("/housing/create")
-	public String create(@Valid HousingRequest14 housingRequest14, BindingResult bindingResult, Model model) {
+	@RequestMapping("/housing/houseResistration14")
+	public String create(@Validated HousingRequest14 housingRequest14, BindingResult bindingResult, Model model) {
 		//入力判定入れること
 		if (bindingResult.hasErrors()) {
             // バリデーションエラーがある場合の処理
+		     // エラーメッセージを取得
+	        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+	        // エラーメッセージをModelにセット
+	        model.addAttribute("errors", fieldErrors);
             return "housingRequest14"; // エラー表示
-        } else {
-	}
+		}
 		// ユーザー情報の登録2行実装
         model.addAttribute("housingRequest14", housingRequest14);
         return "houseResistration14"; // 成功ページに遷移（現ページにとどまる）
      }
-	}
+}
--- TeamDevelopment_2307/src/main/java/com/example/demo/repository/HousingRepository14.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/repository/HousingRepository14.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HousingRepository14 extends JpaRepository<User, Long>{}
+import com.example.demo.entity.HousingEntity14;
+
+public interface HousingRepository14 extends JpaRepository<HousingEntity14, Long>{}
 
