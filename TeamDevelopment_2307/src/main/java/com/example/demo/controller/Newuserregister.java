ba6e80c468206d2fce42b4982bf4b7301baba852
FurukawaYamato <133792870+FurukawaYamato@users.noreply.github.com>
2023-07-25 21:08:37 +0900

    7/25更新

---
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/Newuserregister.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
+public class Newuserregister {
+    //ログイン画面への遷移
+    @GetMapping
+    String getLogin() {
+        return "login";
+    }
+
+    //ログイン成功時のメニュー画面への遷移
+    @PostMapping
+    String postLogin() {
+        return "redirect:/menu";
+    }
+}
