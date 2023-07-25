7e804b875ecd0b51b571ba69fd29741197aa4781
tomitaterumi <t.tomita2315@gmail.com>
2023-07-25 20:36:00 +0900

    java

--- TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.CusDetEntity;
 import com.example.demo.service.CusDetService;
@@ -11,11 +12,15 @@ import com.example.demo.service.CusDetService;
 @Controller
 
 public class CusDetController {
-		
-	@RequestMapping(value="/housing")
-	public String display(Model model){
-		model.addAttribute("");
-		return "CustomerDetailsS09";
-	}
 
+	@Autowired
+	private CusDetService cusdetService;
+	
+	  @GetMapping("/housing/{id}")
+	  public String displayView(@PathVariable Long id, Model model) {
+	    CusDetEntity user = cusdetService.findById(id);
+	    model.addAttribute("userData", user);
+	    return "housing/CustomerDetailsS09";
+
+	  }
 }
