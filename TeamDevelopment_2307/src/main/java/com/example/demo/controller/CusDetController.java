8cff223567458d972fa1fa0caca8391cb4770997
tomitaterumi <t.tomita2315@gmail.com>
2023-07-27 22:34:31 +0900

    java

--- TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
@@ -1,32 +1,35 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.demo.entity.CusDetEntity;
-import com.example.demo.service.CusDetService;
+//import com.example.demo.entity.CusDetEntity;
+//import com.example.demo.service.CusDetService;
 
 @Controller
-
 public class CusDetController {
 
-	@Autowired
+/*	@Autowired
 	private CusDetService cusdetService;
 	
-	
-	
-	/*@GetMapping(value = "housing/{id}",params = "id = 1")*/
+	//@GetMapping(value = "housing/{id}",params = "id = 1")
 	
 	@RequestMapping(value = "housing/{id}", method = RequestMethod.GET, params = "id=1")
-	public String displayView(@PathVariable Long id, Model model) {
+	public String display(@PathVariable("id") Long id, Model model) {
 		CusDetEntity cusentity = cusdetService.findById(id);
 		model.addAttribute("cusData", cusentity);
 		return "housing/CustomerDetailsS09";
 	  }
-
+*/
+	
+	@GetMapping("/housing/CustomerDetailsS09")
+	public String test(Model model) {
+		return "CustomerDetailsS09";
+	}
 }
+
