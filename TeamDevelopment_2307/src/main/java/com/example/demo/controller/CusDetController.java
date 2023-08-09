e0fa7bc9ee433b895f47f17ddb66652b57ed4103
tomitaterumi <t.tomita2315@gmail.com>
2023-08-09 13:18:26 +0900

    java

--- TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/CusDetController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,21 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//import com.example.demo.entity.CusDetEntity;
-//import com.example.demo.service.CusDetService;
+import com.example.demo.entity.CusDetEntity;
+import com.example.demo.service.CusDetService;
 
 @SpringBootApplication
 @Controller
 public class CusDetController {
 
-//	@Autowired
-//	private CusDetService cusdetService;
+	@Autowired
+	private CusDetService cusdetService;
 	
 
 	@GetMapping(value="housing/CustomerDetailsS09/{id}", params="id=1")
 	public String display(@PathVariable("id") Long id, Model model) {
-//		CusDetEntity cusentity = cusdetService.findById(id);
-//		model.addAttribute("cusData", cusentity);
+		CusDetEntity cusentity = cusdetService.findById(id);
+		model.addAttribute("cusData", cusentity);
 		return "housing/CustomerDetailsS09";
 	  }
 
