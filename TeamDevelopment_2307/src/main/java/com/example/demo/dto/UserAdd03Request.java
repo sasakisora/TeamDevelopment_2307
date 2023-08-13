85f8095d61da26d0f01e0765743880632cf7a297
ManamiHayakawa <133200318+ManamiHayakawa@users.noreply.github.com>
2023-08-13 11:41:48 +0900

    8.13

--- TeamDevelopment_2307/src/main/java/com/example/demo/dto/UserAdd03Request.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/dto/UserAdd03Request.java
@@ -2,7 +2,7 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -15,12 +15,13 @@ public class UserAdd03Request implements Serializable {
   /**
    * ユーザーネーム
    */
-  @NotEmpty(message = "名前を入力してください")
-  @Size(max = 100, message = "名前は100桁以内で入力してください")
+  @NotBlank(message = "名前を入力してください")
+  @Size(max = 10, message = "名前は10桁以内で入力してください")
   private String username;
   /**
    * パスワード
    */
-  @Size(max = 255, message = "正しいパスワードの形で入力してください。")
+  @NotBlank(message = "パスワードを入力してください")
+  @Size(max = 10, message = "パスワードは10桁以内で入力してください。")
   private String password;
 }
