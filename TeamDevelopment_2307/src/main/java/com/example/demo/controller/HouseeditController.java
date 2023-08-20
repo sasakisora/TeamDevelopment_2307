d0d56acb18b0b14d4ac8e5f0aa9c2dc2d7f815dc
tsukamotoarisa <a2qua8@gmail.com>
2023-08-20 12:01:05 +0900

    0820(仮)

--- TeamDevelopment_2307/src/main/java/com/example/demo/controller/HouseeditController.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/controller/HouseeditController.java
@@ -1,14 +1,62 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.HouseEntity;
+import com.example.demo.form.HouseeditForm;
+import com.example.demo.service.HouseeditService;
 
 @Controller
 public class HouseeditController {
+  
+    @Autowired
+    private HouseeditService houseeditService;
 
-    @GetMapping("/")
-    public String displayList(Model model) {
-        return "housing/houseEdit";
+    @GetMapping(value = "/housing/houseEdit/{id}")
+    public String displayEdit(@PathVariable Long id, Model model) {
+        HouseEntity houseEntity = houseeditService.findById(id);
+        HouseeditForm houseeditForm = new HouseeditForm();
+        houseeditForm.setName(houseEntity.getName());
+        houseeditForm.setAddress(houseEntity.getAddress());
+        houseeditForm.setLand_area(houseEntity.getLand_area());
+        houseeditForm.setLayout(houseEntity.getLayout());
+        houseeditForm.setAge(houseEntity.getAge());
+        houseeditForm.setPrice(houseEntity.getPrice());
+        model.addAttribute("HouseeditForm", houseeditForm);
+        return "housing/houseEdit"; // houseEdit.htmlを表示
+    }
+
+    @PostMapping("/houseEdit/update")
+    public String update(@Validated @ModelAttribute HouseeditForm houseeditForm, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError error : result.getAllErrors()) {
+                errorList.add(error.getDefaultMessage());
+            }
+            model.addAttribute("validationError", errorList);
+            model.addAttribute("HouseeditForm", houseeditForm);
+            return "housing/houseEdit"; // エラーがある場合は編集画面に戻る
+        }
+
+        houseeditService.update(houseeditForm);
+        return "redirect:/housing/housesearch"; // 成功時は一覧画面にリダイレクト
+    }
+
+    @GetMapping("/housing/{id}/delete")
+    public String delete(@PathVariable Long id, Model model) {
+        houseeditService.delete(id);
+        return "redirect:/housing/housesearch"; // リダイレクトすることをおすすめします
     }
 }
--- TeamDevelopment_2307/src/main/java/com/example/demo/service/HouseeditService.java
+++ TeamDevelopment_2307/src/main/java/com/example/demo/service/HouseeditService.java
@@ -1,27 +1,58 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.HouseEntity;
+import com.example.demo.form.HouseeditForm;
 import com.example.demo.repository.HouseeditRepository;
 
 /**
  * ユーザー情報 Service
  */
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class HouseeditService {
+ 
+  @Autowired
+  private HouseeditRepository houseeditRepository;
 
-    /**
-     * ユーザー情報 Repository
-     */
-    @Autowired
-    HouseeditRepository houseeditRepository;
 
-    public List<HouseEntity> searchAll() {
-        // ユーザーTBLの内容を全検索
-        return houseeditRepository.findAll();
-    }
+  public List<HouseEntity> searchAll() {
+    return houseeditRepository.findAll();
+  }
+
+
+  public HouseEntity findById(Long id) {
+    return houseeditRepository.findById(id).get();
+  }
+
+
+  public void update(HouseeditForm houseeditForm) {
+	    Date now = new Date();
+	    HouseEntity houseEntity = findById(houseeditForm.getId());
+	    houseEntity.setId(houseeditForm.getId());
+	    houseEntity.setName(houseeditForm.getName());
+	    houseEntity.setAddress(houseeditForm.getAddress());
+	    houseEntity.setLand_area(houseeditForm.getLand_area());
+	    houseEntity.setLayout(houseeditForm.getLayout());
+	    houseEntity.setAge(houseeditForm.getAge());
+	    houseEntity.setPrice(houseeditForm.getPrice());
+	    houseEntity.setCreateDate(now);
+	    houseEntity.setUpdateDate(now);
+	    
+	    houseeditRepository.save(houseEntity);
+  }
+  
+  public void delete(Long id) {
+	  HouseEntity houseEntity = findById(id);
+	  houseeditRepository.delete(houseEntity);
+  }
+  
+  
+
 }
